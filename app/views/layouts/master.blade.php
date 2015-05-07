eef3ec5c014942eef8045dbb6206eb228a875ac0
Pablo Pinero <pinerop@gmail.com>
2015-05-07 18:39:43 -0500

    testing

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -17,7 +17,7 @@
 			/
 			<a href="{{{ action('HomeController@showPortfolio') }}}"> portfolio</a>
 			/
-			<a href="{{{ action('HomeController@showPubs') }}}">publications</a>
+			<a href="{{{ action('HomeController@showPubs') }}}">publicationsTEST</a>
 			@if(Auth::check())
 			/
 			<a href="/logout">logout</a>
