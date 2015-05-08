1b8e1c2208851b760c1b1fb4aab5d578581532b9
Pablo Pinero <pinerop@gmail.com>
2015-05-08 14:45:05 -0500

    added content to nonfiction view

--- app/views/nonfiction.blade.php
+++ app/views/nonfiction.blade.php
@@ -5,9 +5,33 @@
 		<h1>nonfiction</h1>
 		<div class='port-links'>
 			<br>
+			<p class='print-only'>"two stories from the starbucks 
+				in las lomas" {print}<p>
+			<p>the cossack review, january 2014</p>
+			<br>
+			<p class='print-only'>"if i were a soccer coach this is the
+				type of soccer coach i would be" {print}<p>
+			<p>puerto del sol, summer 2013</p>
+			<br>
+			<p class='print-only'>"mirror: a found essay from
+				the twitterverse" {print}<p>
+			<p>passages north, fall 2013</p>
+			<br>
+			<p><a href="http://brevitymag.com/issues/march-2013/twitterverse/"</a>"life, love, happiness: a found essay
+				from the twitterverse"</p>
+			<p>brevity, spring 2013</p>
+			<br>
+			<p class='print-only'>"i want to be someone better than me" {print}<p>
+			<p>juked, spring 2013</p>
+			<br>
+			<p class='print-only'>"how i saved the indians" {print}<p>
+			<p>fourth genre, fall 2012</p>
+			<br>
+			<a href="http://therumpus.net/2012/08/flop-chaos-tragedy-and-the-un-american-beauty-of-soccer/">"flop: chaos, tragedy and the (un-american) beauty of soccer"</a>
+			<p>the rumpus, august 2012</p>
+			<br>
 			<a href="http://thenormalschool.com/what-500-days-of-summer-means-to-me-by-pablo-pinero-stillmann/">"what (500) days of summer means to me"</a>
 			<p>the normal school, fall 2012</p>
-			<br>
 			<br>
 		</div>
 	</div>
